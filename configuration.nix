02fb1468d77f48b39c0afdd57a9413cc7a72efa5
floork <floork@gmx.de>
2023-08-14 20:14:06 +0200

    updated files

--- configuration.nix
+++ configuration.nix
@@ -107,7 +107,7 @@
       "	bluez_monitor.properties = {\n		[\"bluez5.enable-sbc-xq\"] = true,\n		[\"bluez5.enable-msbc\"] = true,\n		[\"bluez5.enable-hw-volume\"] = true,\n		[\"bluez5.headset-roles\"] = \"[ hsp_hs hsp_ag hfp_hf hfp_ag ]\"\n	}\n";
   };
 
-  # services.flatpak.enable = true;
+  services.flatpak.enable = true;
   services.gnome.gnome-keyring.enable = true;
 
   # Enable Fish as the default shell.
