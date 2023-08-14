1b9d9f527bf205b49429acb449d3bb90686ba9d4
floork <floork@gmx.de>
2023-08-14 20:16:28 +0200

    fixed flatpak

--- configuration.nix
+++ configuration.nix
@@ -107,6 +107,7 @@
       "	bluez_monitor.properties = {\n		[\"bluez5.enable-sbc-xq\"] = true,\n		[\"bluez5.enable-msbc\"] = true,\n		[\"bluez5.enable-hw-volume\"] = true,\n		[\"bluez5.headset-roles\"] = \"[ hsp_hs hsp_ag hfp_hf hfp_ag ]\"\n	}\n";
   };
 
+  xdg.portal.enable = true;
   services.flatpak.enable = true;
   services.gnome.gnome-keyring.enable = true;
 
