7aa164d211f8365a803eecd904026994ff13bc42
Florian Mros <mail@floork.dev>
2023-09-18 16:06:05 +0200

    updated config

--- modules/fish.nix
+++ modules/fish.nix
@@ -17,6 +17,7 @@ in {
   }];
 
   shellInit = ''
+    # My fish config. Not much to see here; just some pretty standard stuff.
     set -e fish_user_paths
     set -U fish_user_paths $HOME/.local/bin $HOME/Applications $fish_user_paths
 
@@ -30,19 +31,38 @@ in {
     end
 
     set -gx PATH "$HOME/.cargo/bin" $PATH
-
     set fish_greeting ""
     set TERM xterm-256color
     set EDITOR vim
 
+    ### AUTOCOMPLETE AND HIGHLIGHT COLORS ###
     set fish_color_normal brcyan
     set fish_color_autosuggestion '#7d7d7d'
     set fish_color_command brcyan
     set fish_color_error '#ff6c6b'
     set fish_color_param brcyan
 
+    ### FUNCTIONS ###
+
+    function frm
+        read -p "Are you sure you want to delete? (y/n): " -l response
+        if test "$response" = "y" -o "$response" = "Y"
+            command rm -rf $argv
+        else
+            echo "Aborted."
+        end
+    end
+
+    function sudo_prev_command
+        eval (history --search-backward "")
+    end
+
+    ### END OF FUNCTIONS ###
+
     alias reload='source ~/.config/fish/config.fish'
+    alias frm=frm
     alias ex=extract
+    alias !!=sudo_prev_command
 
     # Include all aliases
     [ -f /etc/nixos/configs/aliasrc ] && source /etc/nixos/configs/aliasrc
