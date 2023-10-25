0047caad5a9878485b72f0df77fa2827cb151797
floork <mail@floork.dev>
2023-10-25 12:40:17 +0200

    fix(install): removed dotfiles cloning

--- install.sh
+++ install.sh
@@ -2,5 +2,4 @@ sudo nix-channel --add https://nixos.org/channels/nixos-unstable nixos
 sudo nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
 sudo nix-channel --update
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-git clone https://github.com/floork/dotfiles.git
 sudo nixos-rebuild switch --upgrade
