7b663fd819729b2d60100af34749824348a29950
Florian Mros <florian_alexander.mros@tu-dresden.de>
2024-03-01 14:32:54 +0100

    23.11

--- home-manager/personal.nix
+++ home-manager/personal.nix
@@ -11,5 +11,5 @@
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new Home Manager release introduces breaking changes.
-  home.stateVersion = "24.05";
+  home.stateVersion = "23.11";
 }
