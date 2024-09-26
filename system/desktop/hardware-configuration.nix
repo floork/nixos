e54108eb23e09ac6ff7d266dc8c6ee50e51c8461
Florian Mros <florian_alexander.mros@tu-dresden.de>
2024-09-26 17:12:01 +0200

    update: flakes

--- system/desktop/hardware-configuration.nix
+++ system/desktop/hardware-configuration.nix
@@ -39,11 +39,6 @@
     fsType = "ext4";
   };
 
-  services.zramSwap = {
-    enable = true;
-    compression = "zstd";
-  };
-
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
