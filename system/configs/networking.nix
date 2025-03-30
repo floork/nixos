5797a6eccf23249d4fb470f1fc13ea23a5cdb315
Florian Mros <florian_alexander.mros@tu-dresden.de>
2025-03-30 18:33:51 +0200

    fix(networking.nix): fixed it

--- system/configs/networking.nix
+++ system/configs/networking.nix
@@ -25,21 +25,12 @@
           from = 8000;
           to = 8010;
         }
-        # Open mDNS (UDP 5353) and SLP (UDP 427) ports
-        {
-          from = 5353;
-          to = 5353;
-        }
-        {
-          from = 427;
-          to = 427;
-        }
       ];
     };
     nameservers = [
       "10.42.0.11"
-      "1.1.1.1#one.one.one.one"
-      "1.0.0.1#one.one.one.one"
+      # "1.1.1.1#one.one.one.one"
+      # "1.0.0.1#one.one.one.one"
     ];
   };
 }
